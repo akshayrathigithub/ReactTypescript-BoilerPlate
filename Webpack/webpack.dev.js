5b898bd0790dcae6d8bd66c78dbce41fc534faf1
Akshay Rathi <akshayrathi96@gmail.com>
2021-11-07 14:05:58 +0530

    prettier formating fixes

--- Webpack/webpack.dev.js
+++ Webpack/webpack.dev.js
@@ -1,11 +1,11 @@
-const webpack = require("webpack");
+const webpack = require('webpack')
 
 module.exports = {
-  mode: "development",
-  devtool: "cheap-module-source-map",
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
   devServer: {
     hot: true,
     open: true,
   },
   plugins: [],
-};
+}
--- Webpack/webpack.prod.js
+++ Webpack/webpack.prod.js
@@ -1,7 +1,7 @@
-const webpack = require("webpack");
+const webpack = require('webpack')
 
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'production',
+  devtool: 'source-map',
   plugins: [],
-};
+}
