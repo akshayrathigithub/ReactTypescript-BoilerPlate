90825d9829b6461d0e1c8a1348ea5ca413592355
Akshay Rathi <akshayrathi96@gmail.com>
2022-01-20 17:30:54 +0530

    declaration file updated

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,5 +1,20 @@
 declare module '*.png'
 declare module '*.svg' {
-  const content: string
-  export default content
+  import * as React from 'react'
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >
+
+  const SRC: string
+  export default SRC
+}
+
+declare module '*.jpg'
+declare module '*.pdf'
+
+declare global {
+  interface Window {
+    location: Location
+  }
 }
