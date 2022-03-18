90b809a17c6500dcfe69a067f2464ca96142d9a8
Akshay Rathi <akshayrathi.official@gmail.com>
2022-03-18 12:37:08 +0530

    project setup changes

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 
 const App: React.FC = () => {
-  return <div>This is the App Component !!!</div>
-}
-export default App
+  return <div>This is the App Component !!!</div>;
+};
+export default App;
