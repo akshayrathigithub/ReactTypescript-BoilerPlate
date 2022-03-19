3e9a1f920c278b8dc7912ed5fd86b9a24e332469
Akshay Rathi <akshayrathi.official@gmail.com>
2022-03-19 23:59:53 +0530

    app component updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { List } from './Components/List/List';
+import Toast from './Components/Toast/Toast';
+import { TOAST_TYPE } from './Enum/Enum';
 import './GlobalStyle.scss';
 
 const App: React.FC = () => {
+  const [toast, setToast] = useState({
+    showToast: false,
+    message: '',
+    type: TOAST_TYPE.SUCCESS,
+  });
+
+  const showToast = (message: string, toastType: TOAST_TYPE) => {
+    const updatedState = toast;
+    updatedState.showToast = true;
+    updatedState.type = toastType;
+    updatedState.message = message;
+    setToast({ ...updatedState });
+    setTimeout(() => {
+      updatedState.showToast = false;
+      setToast({ ...updatedState });
+    }, 4000);
+  };
   return (
     <div className="main-app">
-      <List />
+      {toast.showToast && <Toast message={toast.message} type={toast.type} />}
+      <List showToast={(message, type) => showToast(message, type)} />
     </div>
   );
 };
