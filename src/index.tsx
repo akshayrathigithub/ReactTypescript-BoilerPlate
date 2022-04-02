93d2a18dc043f6d6c84a40d1eac1595d9e3575be
Akshay Rathi <akshayrathi.official@gmail.com>
2022-04-03 00:15:16 +0530

    changes required for react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
-import ReactDom from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 
-ReactDom.render(<App />, document.getElementById('rootApp'))
+const container = document.getElementById('rootApp') as HTMLDivElement
+const root = createRoot(container)
+root.render(<App />)
